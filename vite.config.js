48dc8e81679af7633bc51793c154af9f42dfdfbd
SimonSchulz <zenkov241091@gmail.com>
2026-03-06 23:30:58 +0300

    fix problem with vite and svg copy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite';
+import {viteStaticCopy} from "vite-plugin-static-copy";
 
 export default defineConfig({
     root: './',
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
+        assetsInlineLimit: 0,
         rollupOptions: {
             input: './index.html'
         }
-    }
+    },
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'assets/*.svg',
+                    dest: 'assets'
+                }
+            ]
+        })
+    ]
 });
